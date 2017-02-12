9a3ec215770a5620ef33f48ac2428946b298ae60
Clive Chan <cc@clive.io>
2017-02-11 20:38:45 -0500

    react kind of working now, scrapped python in favor of firebase

---
+++ app.jsx
@@ -0,0 +1,48 @@
+var requires = ["Card", "CardImg", "CardText", "CardBlock",
+                "CardTitle", "CardSubtitle", "Button"];
+for(var i = 0; i < requires.length; i++)
+  window[requires[i]] = Reactstrap[requires[i]];
+
+var DifficultySection = React.createClass({
+  render: function() {
+    var dClasses = ["easy", "medium", "hard"];
+    var dTitles = ["Easy", "Med", "Hard"]
+    var currentQuestion = 1; //add to this.state
+    return (
+      <span className={"difficulty "+dClasses[this.props.difficulty]}>
+        <h3>{dTitles[this.props.difficulty]} {currentQuestion}</h3>
+        hello
+      </span>
+    );
+  }
+});
+var TeamCard = React.createClass({
+  render: function() {
+    var title = "Unknown Team";
+    return (
+      <div>
+        <Card inverse style={{ backgroundColor: '#696', borderColor: '#696' }}>
+          <CardBlock>
+            <CardTitle>{title}</CardTitle>
+            <CardSubtitle>ID: <input type="text" placeholder="12345"/></CardSubtitle>
+            <DifficultySection difficulty="0"/>
+            <DifficultySection difficulty="1"/>
+            <DifficultySection difficulty="2"/>
+          </CardBlock>
+        </Card>
+      </div>
+    );
+  }
+});
+var TeamCardSet = React.createClass({
+  render: function() {
+    var cards = [<TeamCard />];
+    return (
+      <CardGroup>{cards}</CardGroup>
+    );
+  }
+});
+ReactDOM.render(
+  <TeamCard />,
+  document.getElementById('app')
+);
