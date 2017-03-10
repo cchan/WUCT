865b0908309ed170e43402dd0a5c09bca5f1c410
Clive Chan <cc@clive.io>
2017-03-09 23:22:04 -0500

    react state storage is working

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -14,27 +14,27 @@ const scoreValues = {
 
 //use IDs: separate page to edit ID=>name associations (up to 50 teams)
 
+//auth is definitely necessary
+
 class RadioBtnGroup extends React.Component {
   constructor(props) {
     super(props);
-    
-    this.state = {};
     
     this.onRadioBtnClick = this.onRadioBtnClick.bind(this);
   }
   onRadioBtnClick(selected) {
     this.setState({ selected: selected });
+    this.props.scoreHandler(selected);
   }
   render() {
     return (
       <div>
         <ButtonGroup>
-          <Button onClick={() => this.onRadioBtnClick(0)} active={this.state.selected === 0}>0</Button>
-          <Button onClick={() => this.onRadioBtnClick(1)} active={this.state.selected === 1}>1</Button>
-          <Button onClick={() => this.onRadioBtnClick(2)} active={this.state.selected === 2}>2</Button>
-          <Button onClick={() => this.onRadioBtnClick(3)} active={this.state.selected === 3}>3</Button>
+          <Button onClick={() => this.onRadioBtnClick(0)} active={this.props.selected === 0}>0</Button>
+          <Button onClick={() => this.onRadioBtnClick(1)} active={this.props.selected === 1}>1</Button>
+          <Button onClick={() => this.onRadioBtnClick(2)} active={this.props.selected === 2}>2</Button>
+          <Button onClick={() => this.onRadioBtnClick(3)} active={this.props.selected === 3}>3</Button>
         </ButtonGroup>
-        <p>Selected: {this.state.selected}</p>
       </div>
     )
   }
@@ -44,19 +44,39 @@ class DifficultySection extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentQuestion: 0
+      currentQuestion: 0,
+      scores: [0, 0, 0]
     };
+  }
+  scoreHandler(selected){
+    var newScores = this.state.scores;
+    newScores[this.state.currentQuestion] = selected;
+    this.setState({scores: newScores});
+  }
+  back(){
+    this.setState({currentQuestion: this.state.currentQuestion - 1});
+  }
+  next(){
+    this.setState({currentQuestion: this.state.currentQuestion + 1});
   }
   render() {
     //fb.child(this.state.id + '/' + this.props.difficulty);
     const dClass = ["easy", "medium", "hard"];
     const dTitle = ["Easy", "Med", "Hard"];
     const numQuestions = [3, 3, 3]; //if currentQuestion hits this then display end screen
+    
+    var buttons = [];
+    if(this.state.currentQuestion > 0)
+      buttons.push();
+    if(this.state.currentQuestion < 2)
+      buttons.push();
+    
     return (
       <span className={"difficulty "+dClass[this.props.difficulty]}>
+        <Button className="back" onClick={this.back.bind(this)} disabled={this.state.currentQuestion <= 0}>&lt;</Button>
+        <Button className="next" onClick={this.next.bind(this)} disabled={this.state.currentQuestion >= 2}>&gt;</Button>
         <h3>{dTitle[this.props.difficulty]} {this.state.currentQuestion+1}</h3>
-        <RadioBtnGroup />
-        <div><Button className="back">&lt;</Button><Button className="next">&gt;</Button></div>
+        <RadioBtnGroup selected={this.state.scores[this.state.currentQuestion]} scoreHandler={this.scoreHandler.bind(this)} />
       </span>
     );
   }
@@ -101,6 +121,6 @@ class TeamCardSet extends React.Component {
 }
 
 ReactDOM.render(
-  <RadioBtnGroup />,
+  <TeamCardSet />,
   document.getElementById('app')
 );
