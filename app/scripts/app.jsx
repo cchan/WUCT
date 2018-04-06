be8a59e8b8a52ee93ad7f0972397ba1bee30a043
Clive Chan <cc@clive.io>
2018-04-06 03:11:55 -0400

    add timer to scoring page (closes #6) and unify some more in common.js

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -201,6 +201,7 @@ class TeamCardSet extends React.Component {
           <header>
             <h1><img src="wuct.jpg" alt="WUCT" />Breaking Bonds Round: Scoring</h1>
             <a href="#" className="add" onClick={this.addCard.bind(this)} title="Press the + button above to start tracking a team! You can:&#013;&bull; Enter a team ID to begin tracking a team's score live&#013;&bull; Set colors to help quickly visually identify teams&#013;&bull; Enter scores by clicking the score (0, 1, 2, or 3).&#013;&bull; Advance to the next question (>) or go back to a previous question (<). The packet difficulty and number shown should always be the one that the team has or can take next."><i className="fa fa-plus" aria-hidden="true"></i></a>
+            <div id="timer" style={{display: "inline-block", margin: "0 1em", fontSize: "1.5em"}}></div>
             <a href="#" className="signout" onClick={function(){window.signOut()}}><i className="fa fa-sign-out" aria-hidden="true"></i></a>
           </header>
           <div>{cards}</div>
@@ -209,6 +210,9 @@ class TeamCardSet extends React.Component {
     }else{
       return <p>Not authenticated</p>;
     }
+  }
+  componentDidMount() {
+    window.setTimer(document.getElementById("timer"));
   }
 }
 
