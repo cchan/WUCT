468ee1a24ca4ea6a627183dccd05421dc5f97642
Clive Chan <cc@clive.io>
2019-02-12 14:36:51 -0500

    bump to 2019

--- app/static/common.js
+++ app/static/common.js
@@ -3,7 +3,7 @@ window.dClass = ["easy", "medium", "hard"];
 window.dTitle = ["Easy", "Med", "Hard"];
 window.numPackets = [20, 15, 9];
 window.scoreValues = [1, 3, 7];
-window.dbName = 'wuct2018';
+window.dbName = 'wuct2019';
 
 if(window.location.hostname != 'wuct.clive.io' && (window.location.hostname != 'localhost' || window.location.port != 8849)) throw new Error();
 
