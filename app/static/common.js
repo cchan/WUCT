81f08e01a7e2dff8fe70c07be2b797c8dc0bff41
Clive Chan <cc@clive.io>
2019-03-24 23:33:16 -0400

    disconnection alert, closes #7

--- app/static/common.js
+++ app/static/common.js
@@ -66,6 +66,43 @@ function setTimer(el, cb){
 }
 
 
+function alertSuccess(text, persistent) {
+    $("#success-alert").hide();
+    $("#failure-alert").hide();
+    $("#success-alert").text(text).fadeTo(2000, 500);
+    if(!persistent)
+      $("#success-alert").slideUp(500, function(){
+         $("#success-alert").slideUp(3000);
+      });
+}
+function alertFailure(text, persistent) {
+    $("#success-alert").hide();
+    $("#failure-alert").hide();
+    $("#failure-alert").text(text).fadeTo(2000, 500);
+    if(!persistent)
+      $("#failure-alert").slideUp(500, function(){
+         $("#failure-alert").slideUp(3000);
+      });
+}
+$(function(){
+  $('body').append(`<div class="alerts" style="position: fixed; top: 4em; left: 1em; right: 1em; height: 4em; pointer-events: none;">
+    <div class="alert alert-success" id="success-alert" style="position:absolute; width:100%; text-align: center;"></div>
+    <div class="alert alert-danger" id="failure-alert" style="position:absolute; width:100%; text-align: center;"></div>
+  </div>`);
+  $("#success-alert").hide();
+  $("#failure-alert").hide();
+});
+
+var connectedRef = firebase.database().ref(".info/connected");
+connectedRef.on("value", function(snap) {
+  if (snap.val() === true) {
+    alertSuccess("connected");
+  } else {
+    alertFailure("not connected", true);
+  }
+});
+
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
